256aac0d590fcc7860c58c32da7b438328ef6584
Tahsin Rahman <tr174@duke.edu>
2025-02-15 23:14:40 -0500

    added avatar

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,5 +1,17 @@
+import Image from "next/image";
+
 const Avatar = () => {
-  return <div>Avatar</div>;
+  return (
+    <div className="hidden xl:flex xl:max-w-none">
+      <Image
+        src="/avatar.png" 
+        width={737}
+        height={678}
+        alt="Avatar"
+        className="translate-z-0"
+      />
+    </div>
+  );
 };
 
 export default Avatar;
