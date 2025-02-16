6d6f42c59fd7051a13f10fce5206916ddfd0c374
Tahsin Rahman <tr174@duke.edu>
2025-02-15 19:51:55 -0500

    Finsihed  transitions

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,27 @@
 import '../styles/globals.css';
-
+// Components 
 import Layout from '../components/Layout';
+import Transition from '../components/Transition';
 
-//components 
+// Router
+import { useRouter } from 'next/router';
+
+// Framer Motion
+import { AnimatePresence, motion } from 'framer-motion';
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
 
   return (
     <Layout>
-      <Component {...pageProps} />;
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className="h-full">
+          <Transition />
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </Layout>
-  )
+  );
 }
 
 export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,10 @@
+import Image from "next/image";
+import ParticlesContainer from '../components/ParticlesContainer';
+import ProjectsBtn from '../components/ProjectsBtn';
+import Avatar from '../components/Avatar';
+
+import {motion} from
+
 const Home = () => {
   return <div>Home</div>;
 };
