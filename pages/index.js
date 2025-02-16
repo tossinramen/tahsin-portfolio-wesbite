25f7c51894763e5dbae44deccfc8dfc65742bbd8
Tahsin Rahman <tr174@duke.edu>
2025-02-15 22:55:49 -0500

    working with img

--- pages/index.js
+++ pages/index.js
@@ -11,12 +11,21 @@ const Home = () => {
     <div className="bg-primary/60 h-full">
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
-          <h1 className="h1">
+          <motion.h1 
+          variants={fadeIn("down", 0.4)}
+          initial="hidden"
+          animate="show" 
+          exit="hidden"
+          className="h1">
             Transforming Ideas <br /> Into{" "}
             <span className="text-accent">Digital Reality</span>
-          </h1>
+          </motion.h1>
 
-          <motion.p variants={fadeIn("down", 0.4)} initial="hidden" animate="show" exit="hidden" className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">asdasdda
+          <motion.p
+          variants={fadeIn("down", 0.4)}
+          initial="hidden"
+          animate="show" 
+          exit="hidden" className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">asdasdda
 
           </motion.p>
 
@@ -34,7 +43,11 @@ const Home = () => {
       </div>
 
       {/* Placeholder for Image */}
-      <div>image</div>
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full">
+          image
+        </div>
+      </div>
     </div>
   );
 };
