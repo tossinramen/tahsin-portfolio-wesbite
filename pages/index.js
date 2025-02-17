ac39c5dca3875e835b1808254df223db9f55c040
Tahsin Rahman <tr174@duke.edu>
2025-02-16 22:51:22 -0500

    did particles

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ const Home = () => {
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           <motion.h1 
-          variants={fadeIn("down", 0.4)}
+          variants={fadeIn("down", 0.2)}
           initial="hidden"
           animate="show" 
           exit="hidden"
@@ -22,7 +22,7 @@ const Home = () => {
           </motion.h1>
 
           <motion.p
-          variants={fadeIn("down", 0.4)}
+          variants={fadeIn("down", 0.3)}
           initial="hidden"
           animate="show" 
           exit="hidden" className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-10 xl:mb-16">asdasdda
@@ -47,10 +47,16 @@ const Home = () => {
         <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0">
 
         </div>
-        <div>
-          Particles
-        </div>
+        <ParticlesContainer />
+        <motion.div
+        variants={fadeIn("down", 0.5)}
+        initial="hidden"
+        animate="show" 
+        exit="hidden"
+        transition={{duration:1, ease: 'easeInOut'}}
+        className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
         < Avatar/>
+        </motion.div>
       </div>
     </div>
   );
