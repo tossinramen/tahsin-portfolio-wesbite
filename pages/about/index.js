ab5c599745da2abc8ff2a31d67293b1b3a4e976d
Tahsin Rahman <tr174@duke.edu>
2025-02-17 16:59:41 -0500

    circles, about index.js

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,3 +1,5 @@
+import React, { useState } from 'react';
+
 // icons
 import {
   FaHtml5,
@@ -14,6 +16,7 @@ import {
   SiAdobexd,
   SiAdobephotoshop,
 } from "react-icons/si";
+import Avatar from '../../components/Avatar';
 
 
 //  data
@@ -88,8 +91,43 @@ const aboutData = [
   },
 ];
 
+
+import Circles from '../../components/Circles';
+
+
+
+
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
+
 const About = () => {
-  return <div>About</div>;
+  const [index, setIndex] = useState(0);
+  console.log(index)
+
+  return <div className = 'h-full bg-primary/30 py-32 text-center xl:text-left'>
+    <Circles />
+    <motion.div variants={fadeIn('right', 0.2)} initial='hidden' animate ='show' exit = 'hidden' className='hidden xl:flex absolute bottom-0 -left-[370px]'>
+      <Avatar />
+    </motion.div>
+    <div className='container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6'>
+      <div>text</div>
+      <div>
+          <div className='flex gap-x-4 xl:gap-x-8 mx-auto xl:mx-0 mb-4'>
+          {aboutData.map((item, itemIndex) => (
+  <div
+    key={itemIndex}
+    className={`cursor-pointer capitalize xl:text-lg relative after:w-8 after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0
+      ${index === itemIndex ? 'text-accent after:w-[100%] after:bg-accent transition-all after:duration-300' : ''}`}
+    onClick={() => setIndex(itemIndex)}
+  >
+    {item.title}
+  </div>
+))}
+
+          </div>
+          </div>
+    </div>
+  </div>;
 };
 
 export default About;
