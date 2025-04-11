c293abd4a44a0af60858213ca04abbc6e0a823cc
Tahsin Rahman <tr174@duke.edu>
2025-04-11 05:20:50 -0400

    updated avatar

--- components/Socials.js
+++ components/Socials.js
@@ -6,15 +6,10 @@ import {RiYoutubeLine, RiInstagramLine, RiFacebookLine, RiDribbbleLine, RiBehanc
 
 const Socials = () => {
   return (<div className="flex items-center gap-x-5 text-lg">
-    <Link href={''} className ='hover:text-accent transition-all duration - 300'>
+    <Link href={'https://www.youtube.com/@tahsinr9622/videos'} className ='hover:text-accent transition-all duration - 300'>
       <RiYoutubeLine />
     </Link>
-    <Link href={''} className ='hover:text-accent transition-all duration - 300'>
-      <RiInstagramLine />
-    </Link>
-    <Link href={''} className ='hover:text-accent transition-all duration - 300'>
-      <RiFacebookLine />
-    </Link>
+    
     <Link href={''} className ='hover:text-accent transition-all duration - 300'>
       <RiDribbbleLine />
     </Link>
