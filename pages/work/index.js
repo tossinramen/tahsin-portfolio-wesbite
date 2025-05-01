23fb378aa3ef7128519f4cfeb272cb0735cd68c0
Tahsin Rahman <tr174@duke.edu>
2025-05-01 19:58:21 -0400

    changed some things

--- pages/work/index.js
+++ pages/work/index.js
@@ -7,12 +7,12 @@ import { fadeIn } from '../../variants';
 
 const Work = () => {
   return (
-    <div className="h-full bg-primary/30 py-36 flex items-center">
+    <div className="min-h-screen bg-primary/30 pt-52 md:pt-36 flex items-center">
       <Circles />
       <div className="container mx-auto">
         <div className="flex flex-col xl:flex-row gap-x-16 items-center xl:items-start">
           
-          {/* Left Section: Title & Description */}
+          
           <div className="text-center xl:text-left xl:w-[40%] space-y-6">
             <motion.h2
               variants={fadeIn('up', 0.2)}
@@ -25,26 +25,27 @@ const Work = () => {
             </motion.h2>
 
             <motion.p
-            variants={fadeIn('up', 0.4)}
-            initial="hidden"
-            animate="show"
-            exit="hidden"
-            
-             className="mb-4 max-w-[400px] mx-auto lg:mx-0">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum.
+              variants={fadeIn('up', 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+            >
+              These are some of the projects I've worked on. To see more, check out my <a href="https://github.com/tossinramen" target="_blank" rel="noopener noreferrer" className="font-bold text-accent">GitHub</a>.
             </motion.p>
           </div>
 
-          {/* Right Section: Swiper Slider */}
-          <motion.div 
-          variants={fadeIn('down', 0.6)}
-          initial="hidden"
-          animate="show"
-          exit="hidden"
-          className="w-full xl:max-w-[70%] mt-8 xl:mt-0">
+          
+          <motion.div
+            variants={fadeIn('down', 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[70%] mt-8 xl:mt-0"
+          >
             <WorkSlider />
           </motion.div>
-          
+
         </div>
       </div>
     </div>
